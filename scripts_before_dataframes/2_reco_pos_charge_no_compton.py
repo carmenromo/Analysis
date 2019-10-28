5b93a7569928db49d0ca81fb6e4e5f7a39aa78dc
Carmen <carmenromoluque@gmail.com>
2019-10-28 16:53:40 +0100

    Fix bug in calling table for r thr

--- scripts_before_dataframes/2_reco_pos_charge_no_compton.py
+++ scripts_before_dataframes/2_reco_pos_charge_no_compton.py
@@ -46,7 +46,7 @@ zpos_threshold = 4
 e_threshold    = 2
 
 rpos_file   = f"{base_path}/r_sigma_phi_table_{identifier}_thr{rpos_threshold}pes_no_compton.h5"
-Rpos        = ats.load_rpos(rpos_file, group="Radius", node=f"f{rpos_threshold}pes150bins")
+Rpos        = ats.load_rpos(rpos_file, group="Radius", node=f"f4pes150bins")
 
 #if identifier == 'irad165mm_d3cm_no_refl_sipms':
 #    Rpos = ats.load_rpos(rpos_file, group="Radius", node=f"f4pes150bins")
