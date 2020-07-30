56063e07585a7dad2f0f8530fe4a3747628a91a8
Carmen Romo <carmenromoluque@gmail.com>
2020-07-30 12:10:48 +0200

    Add build_r_map script for nest

---
+++ scripts_dataframes/build_r_map_nest.py
@@ -0,0 +1,141 @@
+import sys
+import argparse
+import numpy  as np
+import pandas as pd
+
+import antea.database.load_db      as db
+import antea.reco.reco_functions   as rf
+import antea.reco.mctrue_functions as mcf
+import reco_functions              as rf2
+
+from antea.io.mc_io import load_mcsns_response
+from antea.io.mc_io import load_mchits
+from antea.io.mc_io import load_mcparticles
+
+
+"""
+python buil_r_map_nest.py 0 1 2 /Users/carmenromoluque/Desktop/ full_body_center_nest_fixed /Users/carmenromoluque/Desktop/
+"""
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_file' , type = int, help = "first file (inclusive)"     )
+    parser.add_argument('n_files'    , type = int, help = "number of files to analize" )
+    parser.add_argument('thr_r'      , type = int, help = "threshold in r coordinate"  )
+    parser.add_argument('events_path',             help = "input files path"           )
+    parser.add_argument('file_name'  ,             help = "name of input files"        )
+    parser.add_argument('data_path'  ,             help = "output files path"          )
+    return parser.parse_args()
+
+def load_sens_pos(file_name):
+    sens_pos = pd.read_hdf(file_name, 'MC/sns_positions')
+    return sens_pos
+
+arguments  = parse_args(sys.argv)
+start      = arguments.first_file
+numb       = arguments.n_files
+threshold  = arguments.thr_r
+eventsPath = arguments.events_path
+file_name  = arguments.file_name
+data_path  = arguments.data_path
+
+evt_file  = f"{data_path}/full_body_r_map_nest_{start}_{numb}_{threshold}"
+
+true_r1, true_r2   = [], []
+var_phi1, var_phi2 = [], []
+var_z1, var_z2     = [], []
+
+touched_sipms1, touched_sipms2 = [], []
+
+for number in range(start, start+numb):
+    #number_str = "{:03d}".format(number)
+    filename  = f"{eventsPath}/{file_name}.{number}.pet.h5"
+    try:
+        sns_response = load_mcsns_response(filename)
+    except ValueError:
+        print(f'File {filename} not found')
+        continue
+    except OSError:
+        print(f'File {filename} not found')
+        continue
+    except KeyError:
+        print(f'No object named MC/waveforms in file {filename}')
+        continue
+    print(f'Analyzing file {filename}')
+
+    sel_df = rf.find_SiPMs_over_threshold(sns_response, threshold)
+
+    particles = load_mcparticles(filename)
+    hits      = load_mchits     (filename)
+    events    = particles.event_id.unique()
+
+    DataSiPM     = load_sens_pos(filename)
+    DataSiPM     = DataSiPM.rename(columns={"sensor_id": "SensorID","x": "X", "y": "Y", "z": "Z"})
+    DataSiPM_idx = DataSiPM.set_index('SensorID')
+
+    for evt in events:
+        ### Select photoelectric events only
+        evt_parts = particles[particles.event_id == evt]
+        evt_hits  = hits     [hits     .event_id == evt]
+        select, true_pos = rf2.true_photoelect_new_nexus(evt_parts, evt_hits)
+        if not select: continue
+
+        waveforms = sel_df[sel_df.event_id == evt]
+        if len(waveforms) == 0: continue
+
+        _, _, pos1, pos2, q1, q2 = rf.assign_sipms_to_gammas(waveforms, true_pos, DataSiPM_idx)
+
+        if len(pos1) > 0:
+            pos_phi    = rf.from_cartesian_to_cyl(np.array(pos1))[:,1]
+            _, var_phi = rf.phi_mean_var(pos_phi, q1)
+
+            pos_z  = np.array(pos1)[:,2]
+            mean_z = np.average(pos_z, weights=q1)
+            var_z  = np.average((pos_z-mean_z)**2, weights=q1)
+
+            reco_cart = np.average(pos1, weights=q1, axis=0)
+            r = np.sqrt(true_pos[0][0]**2 + true_pos[0][1]**2)
+
+            true_r1       .append(r)            
+            var_phi1      .append(var_phi)
+            var_z1        .append(var_z)
+            touched_sipms1.append(len(pos1))
+        else:
+            var_phi1      .append(1.e9)
+            var_z1        .append(1.e9)
+            touched_sipms1.append(1.e9)
+            true_r1       .append(1.e9)
+
+        if len(pos2) > 0:
+            pos_phi    = rf.from_cartesian_to_cyl(np.array(pos2))[:,1]
+            _, var_phi = rf.phi_mean_var(pos_phi, q2)
+
+            pos_z  = np.array(pos2)[:,2]
+            mean_z = np.average(pos_z, weights=q2)
+            var_z  = np.average((pos_z-mean_z)**2, weights=q2)
+
+            reco_cart = np.average(pos2, weights=q2, axis=0)
+            r = np.sqrt(true_pos[1][0]**2 + true_pos[1][1]**2)
+
+            true_r2       .append(r)
+            var_phi2      .append(var_phi)
+            var_z2        .append(var_z)
+            touched_sipms2.append(len(pos2))
+        else:
+            var_phi2      .append(1.e9)
+            var_z2        .append(1.e9)
+            touched_sipms2.append(1.e9)
+            true_r2       .append(1.e9)
+
+a_true_r1  = np.array(true_r1)
+a_true_r2  = np.array(true_r2)
+a_var_phi1 = np.array(var_phi1)
+a_var_phi2 = np.array(var_phi2)
+a_var_z1   = np.array(var_z1)
+a_var_z2   = np.array(var_z2)
+
+a_touched_sipms1 = np.array(touched_sipms1)
+a_touched_sipms2 = np.array(touched_sipms2)
+
+np.savez(evt_file, a_true_r1=a_true_r1, a_true_r2=a_true_r2, a_var_phi1=a_var_phi1, a_var_phi2=a_var_phi2, a_var_z1=a_var_z1, a_var_z2=a_var_z2, a_touched_sipms1=a_touched_sipms1, a_touched_sipms2=a_touched_sipms2)
--- scripts_dataframes/reco_functions.py
+++ scripts_dataframes/reco_functions.py
@@ -41,3 +41,42 @@ def true_photoelect(evt_parts, evt_hits):
     #   return (False, [])
 
     return (True, true_pos)
+
+
+def true_photoelect_new_nexus(evt_parts, evt_hits):
+    """Returns the position of the true photoelectric energy deposition
+    calculated with barycenter algorithm.
+    """
+
+    sel_volume   = (evt_parts.initial_volume == 'ACTIVE') & (evt_parts.final_volume == 'ACTIVE')
+    sel_name     =  evt_parts.particle_name == 'e-'
+    sel_vol_name = evt_parts[sel_volume & sel_name]
+    ids          = sel_vol_name.particle_id.values
+
+    sel_hits   = mcf.find_hits_of_given_particles(ids, evt_hits)
+    energies   = sel_hits.groupby(['particle_id'])[['energy']].sum()
+    energies   = energies.reset_index()
+    energy_sel = energies[rf.greater_or_equal(energies.energy, 0.476443, allowed_error=1.e-6)]
+
+    sel_vol_name_e  = sel_vol_name[sel_vol_name.particle_id.isin(energy_sel.particle_id)]
+
+    primaries = evt_parts[evt_parts.primary == True]
+    sel_all   = sel_vol_name_e[sel_vol_name_e.mother_id.isin(primaries.particle_id.values)]
+    if len(sel_all) == 0:
+        return (False, [])
+
+    ### Once the event has passed the selection, let's calculate the true position(s)
+    ids      = sel_all.particle_id.values
+    sel_hits = mcf.find_hits_of_given_particles(ids, evt_hits)
+
+    sel_hits = sel_hits.groupby(['particle_id'])
+    true_pos = []
+    for _, df in sel_hits:
+        hit_positions = np.array([df.x.values, df.y.values, df.z.values]).transpose()
+        true_pos.append(np.average(hit_positions, axis=0, weights=df.energy))
+
+    ### Reject events where the two gammas have interacted in the same hemisphere.
+    #if (len(true_pos) == 1) & (evt_hits.energy.sum() > 0.511):
+    #   return (False, [])
+
+    return (True, true_pos)
