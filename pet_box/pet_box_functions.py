495d45040895accad25490485df9fea65116265b
carmenromo <carmenromoluque@gmail.com>
2021-03-01 19:24:47 +0100

    Add function for the high energy gamma

--- pet_box/pet_box_functions.py
+++ pet_box/pet_box_functions.py
@@ -26,6 +26,38 @@ def info_from_sensors_with_neg_z(DataSiPM_idx, evt_sns):
     return sns_ids[sel], sns_pos[sel], sns_charges[sel]
 
 
+def select_gamma_high_energy(evt_parts, evt_hits):
+    sel_volume   = (evt_parts.initial_volume == 'ACTIVE') & (evt_parts.final_volume == 'ACTIVE')
+    sel_name     =  evt_parts.particle_name == 'e-'
+    sel_vol_name = evt_parts[sel_volume & sel_name]
+
+    gamma_he  = evt_parts[evt_parts.kin_energy == 1.274537]
+    sel_mother = sel_vol_name[sel_vol_name.mother_id.isin(gamma_he.particle_id.values)]
+
+    ids      = sel_mother.particle_id.values
+    sel_hits = mcf.find_hits_of_given_particles(ids, evt_hits)
+    energies = sel_hits.groupby(['particle_id'])[['energy']].sum()
+    energies = energies.reset_index()
+
+    energy_sel = energies[energies.energy == energies.energy.max()]
+    sel_all = sel_mother[sel_mother.particle_id.isin(energy_sel.particle_id)]
+
+    if len(sel_all)==0:
+        return (False, [])
+
+    ### Once the event has passed the selection, let's calculate the true position(s)
+    ids      = sel_all.particle_id.values
+    sel_hits = mcf.find_hits_of_given_particles(ids, evt_hits)
+
+    sel_hits = sel_hits.groupby(['particle_id'])
+    true_pos = []
+    for _, df in sel_hits:
+        hit_positions = np.array([df.x.values, df.y.values, df.z.values]).transpose()
+        true_pos.append(np.average(hit_positions, axis=0, weights=df.energy))
+
+    return (True, np.array(true_pos))
+
+
 # def select_phot_pet_box(evt_parts, evt_hits):
 #     sel_volume   = (evt_parts.initial_volume == 'ACTIVE') & (evt_parts.final_volume == 'ACTIVE')
 #     sel_name     =  evt_parts.particle_name == 'e-'
