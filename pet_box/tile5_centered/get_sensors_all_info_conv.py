dd22d3ccf818dcc3844d887e884ee85efe3710d8
Carmen Romo <carmenromo@Carmens-MBP.home>
2021-11-30 23:50:01 +0100

    Fix bug and simplify script

--- pet_box/tile5_centered/get_sensors_all_info_conv.py
+++ pet_box/tile5_centered/get_sensors_all_info_conv.py
@@ -17,14 +17,25 @@ python get_sensors_all_info_conv.py 0 1 0 5 /Users/carmenromoluque/nexus_petit_a
 
 print(datetime.datetime.now())
 
-arguments     = pbf.parse_args(sys.argv)
-start         = arguments.first_file
-numb          = arguments.n_files
-thr_ch_start  = arguments.thr_ch_start
-thr_ch_nsteps = arguments.thr_ch_nsteps
-in_path       = arguments.in_path
-file_name     = arguments.file_name
-out_path      = arguments.out_path
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_file'   , type = int, help = "first file (inclusive)"        )
+    parser.add_argument('n_files'      , type = int, help = "number of files to analize"    )
+    parser.add_argument('in_path'      ,             help = "input files path"              )
+    parser.add_argument('file_name'    ,             help = "name of input files"           )
+    parser.add_argument('out_path'     ,             help = "output files path"             )
+    parser.add_argument('variable'     ,             help = "Variable to extract max charge")
+    parser.add_argument('apply_thr'    ,             help = "Apply threshold in charge"     )
+    return parser.parse_args()
+
+arguments = parse_args(sys.argv)
+start     = arguments.first_file
+numb      = arguments.n_files
+in_path   = arguments.in_path
+file_name = arguments.file_name
+out_path  = arguments.out_path
+varible   = arguments.variable
+apply_thr = arguments.apply_thr
 
 
 corona = [11, 12, 13, 14, 15, 16, 17, 18, 21, 28, 31, 38, 41, 48,
@@ -60,15 +71,18 @@ def get_perc_ch_corona(df, variable='charge'):
     return (cor_ch/tot_ch)*100
 
 thr = 9.9 #pes
-evt_file   = f'{out_path}/get_sns_info_conv_thr{thr}_{start}_{numb}'
+evt_file   = f'{out_path}/get_sns_info_conv_{variable}_{apply_thr}_thr{thr}_{start}_{numb}'
 df_sns_resp = pd.DataFrame({})
 for number in range(start, start+numb):
     #number_str = "{:03d}".format(number)
     filename = in_path + f'{file_name}.{number}.h5'
     try:
         sns_response0 = pd.read_hdf(filename, '/conv')
-        #sns_response0 = sns_response0[sns_response0.ToT>0]
-        sns_response0 = sns_response0[sns_response0.charge_mc >= thr]
+        if apply_thr:
+            sns_response0 = sns_response0[sns_response0.charge_mc >= thr]
+        else:
+            sns_response0 = sns_response0[sns_response0.ToT>0]
+
     except OSError:
         print(f'File {filename} does not exist')
         continue
@@ -80,50 +94,17 @@ for number in range(start, start+numb):
 df_coinc = filter_coincidences(df_sns_resp)
 
 ## Coincidences + Centered events Hamamatsu
-df_sns_resp_coinc_cent1 = select_evts_with_max_charge_at_center(df_coinc, variable='charge_data')
-df_sns_resp_coinc_cent2 = select_evts_with_max_charge_at_center(df_coinc, variable='charge_conv')
-df_sns_resp_coinc_cent3 = select_evts_with_max_charge_at_center(df_coinc, variable='charge_mc')
-df_sns_resp_coinc_cent4 = select_evts_with_max_charge_at_center(df_coinc, variable='ToT')
+df_sns_resp_coinc_cent = select_evts_with_max_charge_at_center(df_coinc, variable=variable)
+perc_ch_corona = get_perc_ch_corona(df_sns_resp_coinc_cent, variable=variable)
+df_sns_resp_coinc_cent = df_sns_resp_coinc_cent.set_index(['event_id'])
+#perc_cor --> ratio
+df_sns_resp_coinc_cent['ratio'] = perc_ch_corona[df_sns_resp_coinc_cent.index].values
 
-perc_ch_corona1 = get_perc_ch_corona(df_sns_resp_coinc_cent1, variable='charge_data')
-perc_ch_corona2 = get_perc_ch_corona(df_sns_resp_coinc_cent2, variable='charge_conv')
-perc_ch_corona3 = get_perc_ch_corona(df_sns_resp_coinc_cent3, variable='charge_mc')
-
-df_sns_resp_coinc_cent1 = df_sns_resp_coinc_cent1.set_index(['event_id'])
-df_sns_resp_coinc_cent2 = df_sns_resp_coinc_cent2.set_index(['event_id'])
-df_sns_resp_coinc_cent3 = df_sns_resp_coinc_cent3.set_index(['event_id'])
-
-df_sns_resp_coinc_cent1['perc_cor'] = perc_ch_corona1[df_sns_resp_coinc_cent1.index].values
-df_sns_resp_coinc_cent2['perc_cor'] = perc_ch_corona2[df_sns_resp_coinc_cent2.index].values
-df_sns_resp_coinc_cent3['perc_cor'] = perc_ch_corona3[df_sns_resp_coinc_cent3.index].values
-
-
-np.savez(evt_file, event_id1   =df_sns_resp_coinc_cent1.index,
-                   sensor_id1  =df_sns_resp_coinc_cent1.sensor_id,
-                   charge_data1=df_sns_resp_coinc_cent1.charge_data,
-                   charge_conv1=df_sns_resp_coinc_cent1.charge_conv,
-                   charge_mc1  =df_sns_resp_coinc_cent1.charge_mc,
-                   ToT1        =df_sns_resp_coinc_cent1.ToT,
-                   perc_cor1   =df_sns_resp_coinc_cent1.perc_cor,
-                   event_id2   =df_sns_resp_coinc_cent2.index,
-                   sensor_id2  =df_sns_resp_coinc_cent2.sensor_id,
-                   charge_data2=df_sns_resp_coinc_cent2.charge_data,
-                   charge_conv2=df_sns_resp_coinc_cent2.charge_conv,
-                   charge_mc2  =df_sns_resp_coinc_cent2.charge_mc,
-                   ToT2        =df_sns_resp_coinc_cent2.ToT,
-                   perc_cor2   =df_sns_resp_coinc_cent2.perc_cor,
-                   event_id3   =df_sns_resp_coinc_cent3.index,
-                   sensor_id3  =df_sns_resp_coinc_cent3.sensor_id,
-                   charge_data3=df_sns_resp_coinc_cent3.charge_data,
-                   charge_conv3=df_sns_resp_coinc_cent3.charge_conv,
-                   charge_mc3  =df_sns_resp_coinc_cent3.charge_mc,
-                   ToT3        =df_sns_resp_coinc_cent3.ToT,
-                   perc_cor3   =df_sns_resp_coinc_cent3.perc_cor,
-                   event_id4   =df_sns_resp_coinc_cent4.index,
-                   sensor_id4  =df_sns_resp_coinc_cent4.sensor_id,
-                   charge_data4=df_sns_resp_coinc_cent4.charge_data,
-                   charge_conv4=df_sns_resp_coinc_cent4.charge_conv,
-                   charge_mc4  =df_sns_resp_coinc_cent4.charge_mc,
-                   ToT4        =df_sns_resp_coinc_cent4.ToT)
-
+np.savez(evt_file, event_id   =df_sns_resp_coinc_cent.index,
+                   sensor_id  =df_sns_resp_coinc_cent.sensor_id,
+                   charge_data=df_sns_resp_coinc_cent.charge_data,
+                   charge_conv=df_sns_resp_coinc_cent.charge_conv,
+                   charge_mc  =df_sns_resp_coinc_cent.charge_mc,
+                   ToT        =df_sns_resp_coinc_cent.ToT,
+                   ratio      =df_sns_resp_coinc_cent.ratio)
 print(datetime.datetime.now())
