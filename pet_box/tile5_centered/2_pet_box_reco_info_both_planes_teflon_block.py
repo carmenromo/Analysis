d95141924c7f91b58d33287f3a0c29e4578f0652
carmenromo <carmenromoluque@gmail.com>
2022-06-12 20:09:06 +0200

    Adapt script to teflon block

--- pet_box/tile5_centered/2_pet_box_reco_info_both_planes_teflon_block.py
+++ pet_box/tile5_centered/2_pet_box_reco_info_both_planes_teflon_block.py
@@ -22,27 +22,36 @@ python 2_pet_box_reco_info_both_planes.py 2500 1 /Users/carmenromoluque/nexus_pe
 /Users/carmenromoluque/nexus_petit_analysis/tof_setup/PetBox_analysis/tile5_centered/data_reco_info
 """
 
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('first_file', type = int, help = "first file (inclusive)"    )
+    parser.add_argument('n_files'   , type = int, help = "number of files to analize")
+    parser.add_argument('in_path'   ,             help = "input files path"          )
+    parser.add_argument('file_name' ,             help = "name of input files"       )
+    parser.add_argument('out_path'  ,             help = "output files path"         )
+    return parser.parse_args()
+
 print(datetime.datetime.now())
 
-arguments     = pbf.parse_args_no_ths_and_zpos2(sys.argv)
+arguments     = parse_args(sys.argv)
 start         = arguments.first_file
 numb          = arguments.n_files
 in_path       = arguments.in_path
 file_name     = arguments.file_name
-zpos_file     = arguments.zpos_file
-zpos_file2    = arguments.zpos_file2
+#zpos_file     = arguments.zpos_file
+#zpos_file2    = arguments.zpos_file2
 out_path      = arguments.out_path
 
-int_area = np.array([22, 23, 24, 25, 26, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72, 73, 74, 75, 76, 77,
-                     33, 34, 35, 36, 43, 46, 53, 56, 63, 64, 65, 66, 44, 45, 54, 55])
+# int_area = np.array([22, 23, 24, 25, 26, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72, 73, 74, 75, 76, 77,
+#                      33, 34, 35, 36, 43, 46, 53, 56, 63, 64, 65, 66, 44, 45, 54, 55])
 
-evt_file   = f'{out_path}/pet_box_reco_info_HamVUV_both_planes_{start}_{numb}'
+evt_file   = f'{out_path}/pet_box_reco_info_HamVUV_both_planes_teflon_block_{start}_{numb}'
 
-Zpos = load_map(zpos_file, group="Zpos",
-                            node=f"f2pes200bins",
-                            x_name='Var_x',
-                            y_name='Zpos',
-                            u_name='ZposUncertainty')
+# Zpos = load_map(zpos_file, group="Zpos",
+#                             node=f"f2pes200bins",
+#                             x_name='Var_x',
+#                             y_name='Zpos',
+#                             u_name='ZposUncertainty')
 
 
 timestamp_thr = [0, 0.25, 0.50, 0.75]
@@ -108,72 +117,72 @@ for number in range(start, start+numb):
         if len(qs1)==0 or len(qs2)==0:
             continue
 
-        max_charge_s_id       = ids1[np.argmax(qs1)]
-        max_charge_s_id_tile5 = ids2[np.argmax(qs2)]
+        #max_charge_s_id       = ids1[np.argmax(qs1)]
+        #max_charge_s_id_tile5 = ids2[np.argmax(qs2)]
 
-        if (max_charge_s_id in int_area) and (max_charge_s_id_tile5 in int_area+100):
-            sns_resp1.append(sum(qs1))
+        #if (max_charge_s_id in int_area) and (max_charge_s_id_tile5 in int_area+100):
+        sns_resp1.append(sum(qs1))
 
-            pos_xs1 = np.array(pos1.T[0])
-            mean_x1 = np.average(pos_xs1, weights=qs1)
-            var_xs1 = np.average((pos_xs1 - mean_x1)**2, weights=qs1)
+        pos_xs1 = np.array(pos1.T[0])
+        mean_x1 = np.average(pos_xs1, weights=qs1)
+        var_xs1 = np.average((pos_xs1 - mean_x1)**2, weights=qs1)
 
-            pos_ys1 = np.array(pos1.T[1])
-            mean_y1 = np.average(pos_ys1, weights=qs1)
+        pos_ys1 = np.array(pos1.T[1])
+        mean_y1 = np.average(pos_ys1, weights=qs1)
 
-            z_pos1 = Zpos(var_xs1).value
+        #z_pos1 = Zpos(var_xs1).value
 
-            reco_x1.append(mean_x1)
-            reco_y1.append(mean_y1)
-            reco_z1.append(z_pos1)
+        reco_x1.append(mean_x1)
+        reco_y1.append(mean_y1)
+        reco_z1.append(-53.25)
 
-            event_ids1.append(evt)
+        event_ids1.append(evt)
 
 
-            sns_resp2.append(sum(qs2))
+        sns_resp2.append(sum(qs2))
 
-            pos_xs2 = np.array(pos2.T[0])
-            mean_x2 = np.average(pos_xs2, weights=qs2)
-            var_xs2 = np.average((pos_xs2 - mean_x2)**2, weights=qs2)
+        pos_xs2 = np.array(pos2.T[0])
+        mean_x2 = np.average(pos_xs2, weights=qs2)
+        var_xs2 = np.average((pos_xs2 - mean_x2)**2, weights=qs2)
 
-            pos_ys2 = np.array(pos2.T[1])
-            mean_y2 = np.average(pos_ys2, weights=qs2)
+        pos_ys2 = np.array(pos2.T[1])
+        mean_y2 = np.average(pos_ys2, weights=qs2)
 
-            #### Because planes are symmetric!!
-            z_pos2 = -Zpos(var_xs2).value
+        #### Because planes are symmetric!!
+        #z_pos2 = -Zpos(var_xs2).value
 
-            reco_x2.append(mean_x2)
-            reco_y2.append(mean_y2)
-            reco_z2.append(z_pos2)
+        reco_x2.append(mean_x2)
+        reco_y2.append(mean_y2)
+        reco_z2.append(53.25)
 
-            event_ids2.append(evt)
+        event_ids2.append(evt)
 
-            ## produce a TOF dataframe with convolved time response
-            tof_sns = evt_tof.sensor_id.unique()
-            evt_tof_exp_dist = []
-            for s_id in tof_sns:
-                tdc_conv    = shf.sipm_shaping_convolution(evt_tof, spe_resp, s_id, time_window)
-                tdc_conv_df = shf.build_convoluted_df(evt, s_id, tdc_conv)
-                evt_tof_exp_dist.append(tdc_conv_df)
-            if len(evt_tof_exp_dist) == 0:
-                continue
-            else:
-                evt_tof_exp_dist = pd.concat(evt_tof_exp_dist)
+        ## produce a TOF dataframe with convolved time response
+        tof_sns = evt_tof.sensor_id.unique()
+        evt_tof_exp_dist = []
+        for s_id in tof_sns:
+            tdc_conv    = shf.sipm_shaping_convolution(evt_tof, spe_resp, s_id, time_window)
+            tdc_conv_df = shf.build_convoluted_df(evt, s_id, tdc_conv)
+            evt_tof_exp_dist.append(tdc_conv_df)
+        if len(evt_tof_exp_dist) == 0:
+            continue
+        else:
+            evt_tof_exp_dist = pd.concat(evt_tof_exp_dist)
 
-            ## Calculate different thresholds in charge
-            for k, th in enumerate(timestamp_thr):
-                evt_tof_exp_dist = evt_tof_exp_dist[evt_tof_exp_dist.charge > th/norm]
-                try:
-                    min_id1, min_id2, min_t1, min_t2 = rf.find_coincidence_timestamps(evt_tof_exp_dist, ids1, ids2)
-                except:
-                    min_id1, min_id2, min_t1, min_t2 = -1, -1, -1, -1
+        ## Calculate different thresholds in charge
+        for k, th in enumerate(timestamp_thr):
+            evt_tof_exp_dist = evt_tof_exp_dist[evt_tof_exp_dist.charge > th/norm]
+            try:
+                min_id1, min_id2, min_t1, min_t2 = rf.find_coincidence_timestamps(evt_tof_exp_dist, ids1, ids2)
+            except:
+                min_id1, min_id2, min_t1, min_t2 = -1, -1, -1, -1
 
-                first_sipm1[k].append(min_id1)
-                first_time1[k].append(min_t1)
+            first_sipm1[k].append(min_id1)
+            first_time1[k].append(min_t1)
 
-                first_sipm2[k].append(min_id2)
-                first_time2[k].append(min_t2)
-            event_ids_times.append(evt)
+            first_sipm2[k].append(min_id2)
+            first_time2[k].append(min_t2)
+        event_ids_times.append(evt)
 
 reco_x1 = np.array(reco_x1)
 reco_x2 = np.array(reco_x2)
