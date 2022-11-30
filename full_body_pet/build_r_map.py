54bd422d48e81f1144a59f48392283991069c394
carmenromo <carmenromoluque@gmail.com>
2022-11-30 15:53:39 +0100

    Complete script

--- full_body_pet/build_r_map.py
+++ full_body_pet/build_r_map.py
@@ -45,6 +45,10 @@ evt_file  = f"{data_path}/full_body_r_map_{start}_{numb}_{threshold}"
 true_r1, true_r2   = [], []
 var_phi1, var_phi2 = [], []
 var_z1, var_z2     = [], []
+mean_phi1, mean_phi2 = [], []
+mean_z1, mean_z2     = [], []
+
+evt_ids, num_phot_evts = [], []
 
 touched_sipms1, touched_sipms2 = [], []
 
@@ -81,61 +85,84 @@ for number in range(start, start+numb):
         select, true_pos = rf2.true_photoelect(evt_parts, evt_hits)
         if not select: continue
 
-        waveforms = sel_df[sel_df.event_id == evt]
-        if len(waveforms) == 0: continue
+        sns_resp = sel_df[sel_df.event_id == evt]
+        if len(sns_resp) == 0: continue
 
-        _, _, pos1, pos2, q1, q2 = rf.assign_sipms_to_gammas(waveforms, true_pos, DataSiPM_idx)
+        _, _, pos1, pos2, q1, q2 = rf.assign_sipms_to_gammas(sns_resp, true_pos, DataSiPM_idx)
+
+        evt_ids      .append(evt)
+        num_phot_evts.append(len(true_pos))
 
         if len(pos1) > 0:
             pos_phi    = rf.from_cartesian_to_cyl(np.array(pos1))[:,1]
-            _, var_phi = rf.phi_mean_var(pos_phi, q1)
+            mean_phi, var_phi = rf.phi_mean_var(pos_phi, q1)
 
             pos_z  = np.array(pos1)[:,2]
             mean_z = np.average(pos_z, weights=q1)
             var_z  = np.average((pos_z-mean_z)**2, weights=q1)
 
-            reco_cart = np.average(pos1, weights=q1, axis=0)
             r = np.sqrt(true_pos[0][0]**2 + true_pos[0][1]**2)
 
             true_r1       .append(r)
             var_phi1      .append(var_phi)
             var_z1        .append(var_z)
             touched_sipms1.append(len(pos1))
+            mean_phi1     .append(mean_phi)
+            mean_z1       .append(mean_z)
+        
+        
         else:
             var_phi1      .append(1.e9)
             var_z1        .append(1.e9)
             touched_sipms1.append(1.e9)
             true_r1       .append(1.e9)
+            mean_phi1     .append(1.e9)
+            mean_z1       .append(1.e9)
 
         if len(pos2) > 0:
             pos_phi    = rf.from_cartesian_to_cyl(np.array(pos2))[:,1]
-            _, var_phi = rf.phi_mean_var(pos_phi, q2)
+            mean_phi, var_phi = rf.phi_mean_var(pos_phi, q2)
 
             pos_z  = np.array(pos2)[:,2]
             mean_z = np.average(pos_z, weights=q2)
             var_z  = np.average((pos_z-mean_z)**2, weights=q2)
 
-            reco_cart = np.average(pos2, weights=q2, axis=0)
             r = np.sqrt(true_pos[1][0]**2 + true_pos[1][1]**2)
 
             true_r2       .append(r)
             var_phi2      .append(var_phi)
             var_z2        .append(var_z)
             touched_sipms2.append(len(pos2))
+            mean_phi2     .append(mean_phi)
+            mean_z2       .append(mean_z)
         else:
             var_phi2      .append(1.e9)
             var_z2        .append(1.e9)
             touched_sipms2.append(1.e9)
             true_r2       .append(1.e9)
+            mean_phi2     .append(1.e9)
+            mean_z2       .append(1.e9)
 
-a_true_r1  = np.array(true_r1)
-a_true_r2  = np.array(true_r2)
-a_var_phi1 = np.array(var_phi1)
-a_var_phi2 = np.array(var_phi2)
-a_var_z1   = np.array(var_z1)
-a_var_z2   = np.array(var_z2)
+a_true_r1   = np.array(true_r1)
+a_true_r2   = np.array(true_r2)
+a_mean_phi1 = np.array(mean_phi1)
+a_mean_phi2 = np.array(mean_phi2)
+a_var_phi1  = np.array(var_phi1)
+a_var_phi2  = np.array(var_phi2)
+a_mean_z1   = np.array(mean_z1)
+a_mean_z2   = np.array(mean_z2)
+a_var_z1    = np.array(var_z1)
+a_var_z2    = np.array(var_z2)
+
+a_evt_ids       = np.array(evt_ids)
+a_num_phot_evts = np.array(num_phot_evts)
 
 a_touched_sipms1 = np.array(touched_sipms1)
 a_touched_sipms2 = np.array(touched_sipms2)
 
-np.savez(evt_file, a_true_r1=a_true_r1, a_true_r2=a_true_r2, a_var_phi1=a_var_phi1, a_var_phi2=a_var_phi2, a_var_z1=a_var_z1, a_var_z2=a_var_z2, a_touched_sipms1=a_touched_sipms1, a_touched_sipms2=a_touched_sipms2)
+np.savez(evt_file, true_r1=a_true_r1, true_r2=a_true_r2, 
+var_phi1=a_var_phi1, var_phi2=a_var_phi2, 
+var_z1=a_var_z1, var_z2=a_var_z2, 
+mean_phi1=a_mean_phi1, mean_phi2=a_mean_phi2, mean_z1=a_mean_z1, mean_z2=a_mean_z2,
+evt_ids=a_evt_ids, num_phot_evts=a_num_phot_evts,
+touched_sipms1=a_touched_sipms1, touched_sipms2=a_touched_sipms2)
